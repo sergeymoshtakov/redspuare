228bf361c51439b719dea4636eb0702a48b90fe1
sergeymoshtakov <113889721+sergeymoshtakov@users.noreply.github.com>
2023-06-28 12:55:44 +0200

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            get { return x; }
+            set
+            {
+                if (x != value)
+                {
+                    x = value;
+                    OnPropertyChanged("X");
+                }
+            }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set
+            {
+                if (y != value)
+                {
+                    y = value;
+                    OnPropertyChanged("Y");
+                }
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Захватываем мышь при нажатии на прямоугольник
+            ((Rectangle)sender).CaptureMouse();
+        }
+
+        private void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Перемещаем прямоугольник по окну при перемещении мыши
+            if (((Rectangle)sender).IsMouseCaptured)
+            {
+                var position = e.GetPosition(this);
+                X = position.X;
+                Y = position.Y;
+            }
+        }
+
+        private void Rectangle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            // Отпускаем мышь при отпускании кнопки мыши
+            ((Rectangle)sender).ReleaseMouseCapture();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
